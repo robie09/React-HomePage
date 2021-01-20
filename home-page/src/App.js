e900da5ac3884864ef7530fde09eadb3f5ddb710
Rabab Alalwi <you@example.com>
2021-01-20 12:56:23 +0300

    push work

---
+++ home-page/src/App.js
@@ -0,0 +1,19 @@
+import logo from './';
+import './App.css';
+import cake from 'Cake-Batter-Dessert-Shooters-1-of-1-14.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={cake} className="App-logo" alt="cake" />
+        <p>
+        Sweet cake
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ myshope/src/App.js
@@ -0,0 +1,24 @@
+
+import "./"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={"\images\cake.jpg" }   />
+
+        
+        
+        <div className="s">
+        <p >
+        Sweet cake
+        </p>
+
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
