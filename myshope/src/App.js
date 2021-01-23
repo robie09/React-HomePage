7b2d895c1f1d8c552bd9dbff0f434e3572089fb6
Rabab Alalwi <you@example.com>
2021-01-23 11:22:05 +0300

    add style to shop

--- myshope/src/App.js
+++ myshope/src/App.js
@@ -1,45 +1,22 @@
+import CakeList from "./components/CakeList";
+import Home from "./components/Home";
+import { Globalstyle } from "./style";
+import { ThemeProvider } from "styled-components";
 
-import "./App.css"
-import cakes from "./shop";
-import style from "./style";
-import cakeimage from "./images/cake.jpg"
+const theme = {
+  mainColor: "#242424", // main font color
+  backgroundColor: "#fefafb", // main background color
+  mcolor: "maroon",
+};
 
 function App() {
-  
-
-
-  const cakeslist = cakes.map((cake) => (
-    <ul >
-      <il><img src={cake.image}  style={{width: "20%"}} alt="cakeindex"/></il>
-      <div>
-      <il>{cake.name}</il>
-    </div>
-      <div>
-      <il>Price:{cake.price}</il>
-      </div>
-     
-    </ul>
-    
-  ));
- 
-
   return (
-   
-    
-    <div className="s">
-    <h2>Sweet cake</h2>
-    
-    
-    <p className="cake">
-    There are many different types of cake. Coman to try it </p>
-    
-    <img  style= {style} src={cakeimage}  alt="shop" />
+    <ThemeProvider theme={theme}>
+      <Globalstyle />
 
-
-  <div>{cakeslist}</div>
-
-    </div>
-
+      <Home />
+      <CakeList />
+    </ThemeProvider>
   );
 }
 
